e55ac2c12e5954ac961b55fa92c1c40396a03106
Hajimalung Baba <hajimalung.baba@kony.com>
2018-07-09 12:45:29 +0530

    commiting zip instead root folder.

--- FaceIDSampleApp/jssrc/android/require/userfrmLoginController.js
+++
@@ -1,9 +0,0 @@
-define({
-    //Type your controller code here 
-    onNavigate: function(context) {
-        this.faceIDComp = context;
-        this.faceIDComp.onVerifySuccess = function() {
-            this.view.flxAlert.isVisible = true;
-        }.bind(this);
-    }
-});
